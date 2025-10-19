d538a7dbeba62fb1f6c8c762dd07d04f761ec07e
zxcvlad78 <143466931+zxcvlad78@users.noreply.github.com>
2025-10-20 02:29:21 +0300

    sexsexsexsexsex

--- entities/entity.gd
+++ entities/entity.gd
@@ -5,6 +5,7 @@ var blend_position:Vector2 = Vector2.ZERO
 var unit_velocity:Vector3 = Vector3.ZERO
 
 @export var model:AnimatedModel
+@export var state_machine:SD_NodeStateMachine
 
 func _physics_process(_delta: float) -> void:
 	unit_velocity = velocity.normalized() * transform.basis
